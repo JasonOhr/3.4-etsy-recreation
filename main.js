bb62c1fe8fec616213218cea3792ea5c3142eeae
Jason Ohr <jason.ohr@gmail.com>
2015-05-22 10:08:59 -0400

    Added helper functions to get images and text to work.  They presently do

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 (function(){
-
+    var $productElement = document.querySelector(".products");
     var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=tacos&includes=Images,Shop";
-    fetchJSONP(url, logResults);
+    fetchJSONP(url, app);
 
-    function logResults(httpReturn){
-        httpReturn.results.forEach(function(images){
-            console.log(images.Images[0].url_170x135);
-        })
-        //console.log(httpReturn.results);
+    function app(httpReturn){
+        var products = httpReturn.results;
+
+        displayProducts(products);
+    }
+
+
+    function displayProducts(products){
+        //console.log(products.title);
+        var source   = document.querySelector("#products-template").innerHTML;
+        var template = Handlebars.compile(source);
+        Handlebars.registerHelper('productImage', function(){
+            return this.Images[0].url_170x135;
+        } );
+        Handlebars.registerHelper('checkTitle', function(string){
+            //console.log(string);
+            if(string.length > 30){
+                var s = string.slice(0,30);
+                return s + "...";
+            }else return string;
+        });
+
+        //console.log(html);
+        products.forEach(function(info){
+            var html = template(info);
+
+            $productElement.insertAdjacentHTML('beforeend',html);
+
+        });
+    }
+
+    function HBreturnImag(hello){
+
+        console.log(hello);
+
     }
 
     function fetchJSONP(url, callback) {
