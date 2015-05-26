cef85b4a852ed16ad4b497a3018b9c7bdefce782
Jason Ohr <jason.ohr@gmail.com>
2015-05-25 21:29:57 -0400

    Finished the HTML and CSS for the header as well as the aside. The functionality however, is not complete in either of those two areas

--- main.js
+++ main.js
@@ -1,45 +1,74 @@
 (function(){
+    var categoryObj = {};
+    var categories;
     var $productElement = document.querySelector(".products");
-    var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=tacos&includes=Images,Shop";
+    var $categoriesElement = document.querySelector(".category-list");
+    var url = "https://openapi.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&limit=24&offset=0&keywords=whiskey&includes=Images,Shop";
     fetchJSONP(url, app);
 
     function app(httpReturn){
         var products = httpReturn.results;
-
-        displayProducts(products);
+        displayProducts(products, $productElement,"#products-template");
+        categories = pluck(products);
+        displayProducts(products, $categoriesElement, "#category-template");
     }
 
-
-    function displayProducts(products){
-        //console.log(products.title);
-        var source   = document.querySelector("#products-template").innerHTML;
-        var template = Handlebars.compile(source);
-        Handlebars.registerHelper('productImage', function(){
-            return this.Images[0].url_170x135;
-        } );
-        Handlebars.registerHelper('checkTitle', function(string){
-            //console.log(string);
-            if(string.length > 30){
-                var s = string.slice(0,30);
-                return s + "...";
-            }else return string;
+    function pluck(products){
+        products.map(function(item){
+            return item.category_path;
         });
+    }
 
-        //console.log(html);
-        products.forEach(function(info){
+    function displayProducts(items, element, templateID){
+        //console.log(templateID);
+        var source   = document.querySelector(templateID).innerHTML;
+
+        var template = Handlebars.compile(source);
+
+        items.forEach(function(info){
+
+            //This drops each object from products into the template which
+            //then places everything in there proper spots
             var html = template(info);
 
-            $productElement.insertAdjacentHTML('beforeend',html);
+            //It then adds each block of templated HTML into the page.
 
+            element.insertAdjacentHTML('beforeend',html);
         });
     }
+    //function displayCategories(items, element, templateID){
+    //    var source = document.querySelector(templateID).innerHTML;
+    //    var template = Handlebars.compile(source);
+    //
+    //    items.forEach(function(stuff){
+    //        var html = template(stuff);
+    //        element.insertAdjacentHTML('beforeend', html)
+    //    });
+    //}
 
-    function HBreturnImag(hello){
+    Handlebars.registerHelper('productImage', HBreturnImage );
+    Handlebars.registerHelper('categoryList', HBreturnCategory);
 
-        console.log(hello);
+    function HBreturnImage(){
+        if (this.Images[0].url_170x135) {
+            return this.Images[0].url_170x135;
+        }else return "Image not Found";
 
     }
 
+    function HBreturnCategory(){
+        var item = this.category_path[0];
+        console.log(categoryObj,item);
+        if (categoryObj.hasOwnProperty(item)){
+            console.log("here");
+            return;
+        }else {
+            categoryObj[item] = categoryObj[item] + 1 || 0;
+
+            return new Handlebars.SafeString("<li>" + item + "</li>") ;
+        }
+
+    }
     function fetchJSONP(url, callback) {
         var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
         var script = document.createElement('script');
